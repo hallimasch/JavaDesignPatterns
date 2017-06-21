66b14eea62fe1c7b9e05bb015c561c95dfad1169
Shroom <hallimasch@users.noreply.github.com>
2017-06-21 17:04:12 +0200

    minor change

--- src/factoryPattern/FactoryTest.java
+++ src/factoryPattern/FactoryTest.java
@@ -3,9 +3,14 @@ package factoryPattern;
 public class FactoryTest {
 	public static void main(String[] args){
 		HumanFactory factory = new HumanFactory();
-		Human male = factory.createHuman("male");
-		male.eat();
-		male.sleep();
-		male.beat();
+		Human boy = factory.createHuman("male");
+		boy.eat();
+		boy.sleep();
+		boy.beat();
+		System.out.println("");
+		Human girl = factory.createHuman("female");
+		girl.eat();
+		girl.sleep();
+		girl.beat();
 	}
 }
